f8f4ec421a8885ea61f9204faf3406bab9471d35
Nastya125 <nastyatrue@mail.ru>
2022-11-12 12:37:26 +0300

    simple pomodoro timer is ready

---
+++ script.js
@@ -0,0 +1,82 @@
+const startButton = document.querySelector("#pomodoro-start");
+const pauseButton = document.querySelector('#pomodoro-pause');
+const resetButton = document.querySelector('#pomodoro-reset');
+
+const workMinuts = document.getElementById('w_minutes');
+const workSeconds = document.getElementById('w_seconds');
+
+const breakMinuts = document.getElementById('b_minutes');
+const breakSeconds = document.getElementById('b_seconds');
+
+let counter = document.getElementById('counter');
+
+let startTimer;
+
+startButton.addEventListener('click', () => {
+    if(startTimer === undefined){
+        startTimer = setInterval(timer, 1000)
+    } else {
+        alert("Таймер уже запущен")
+    }
+});
+
+resetButton.addEventListener('click', () => {
+    workMinuts.innerText = 25;
+    workSeconds.innerText = "00"; 
+
+    breakMinuts.innerText = 5;
+    breakSeconds.innerText = "00"
+
+    counter.innerText = 0;
+    stopInterval();
+    startTimer = undefined;
+});
+
+pauseButton.addEventListener('click', () => {
+    stopInterval();
+    startTimer = undefined;
+});
+
+
+function timer(){
+    // Счетчик таймера работы
+    if(workSeconds.innerText != 0){
+        workSeconds.innerText--;
+    } else if(workMinuts.innerText != 0 && workSeconds.innerText == 0) {
+            workSeconds.innerText = 59;
+            workMinuts.innerText--;
+    }
+
+    // Счетчик таймера перерыва
+    if(workSeconds.innerText == 0 && workMinuts.innerText == 0) {
+        if(breakSeconds.innerText != 0) {
+            breakSeconds.innerText--;
+        }  else if(breakMinuts.innerText != 0 && breakSeconds.innerText == 0) {
+            breakSeconds.innerText = 59;
+            breakMinuts.innerText--;
+        }
+    }
+
+    // Счетчик кругов (работа+перерыв)
+    if(workMinuts.innerText == 0 && workSeconds.innerText == 0 && breakMinuts.innerText == 0 && breakSeconds.innerText == 0){
+        workMinuts.innerText = 25;
+        workSeconds.innerText = "00"; 
+
+        breakMinuts.innerText = 5;
+        breakSeconds.innerText = "00"
+
+        counter.innerText++;
+    }
+}
+
+
+function stopInterval() {
+    clearInterval(startTimer)
+}
+
+
+
+
+
+  
+
